4fb82d6b20fb46eaac6ef6e86c1ebc5c45c02a29
John Whitten <johnwhitten.studio@gmail.com>
2022-02-11 20:17:21 -0800

    add program.cs and outputtype line to csproj file

---
+++ PierresBakery/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using PierresBakery.Models;
+
+namespace PierresBakery.Models 
+{
+
+  public class PierresBakery
+  {
+    public static void Main()
+    {
+
+      Console.WriteLine("------------------------------------");
+      Console.WriteLine("~•~•~~•~•~~•~•~///~•~~•~•~~•~•~~•~•~");
+      Console.WriteLine("~•~•~Welcome to Pierre's Bakery~•~•~");
+      Console.WriteLine("~•~•~~•~•~~•~•~///~•~~•~•~~•~•~~•~•~");
+      Console.WriteLine("------------------------------------");
+      Console.WriteLine("---Please have a look at our menu---");
+      Console.WriteLine("------------------------------------");
+
+    }
+  }
+}
